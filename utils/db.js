63e27da5d878bc48cf758cf3a6953ff7407bab8b
AbdulrahmanAbozaid <diver72003@gmail.com>
2024-12-09 19:44:53 +0200

    [MongoClient]: Coded

---
+++ utils/db.js
@@ -0,0 +1,50 @@
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || '27017';
+    this.dbName = process.env.DB_DATABASE || 'files_manager';
+
+    const uri = `mongodb://${host}:${port}`;
+    this.client = new MongoClient(uri, {
+      useUnifiedTopology: true,
+    });
+
+    this.connected = false;
+
+    this.client
+      .connect()
+      .then(() => {
+        this.connected = true;
+      })
+      .catch((err) => {
+        console.error('Error connecting to MongoDB:', err.message);
+      });
+  }
+
+  isAlive() {
+    return this.connected;
+  }
+
+  async nbUsers() {
+    if (!this.isAlive()) {
+      return 0;
+    }
+    const db = this.client.db(this.dbName);
+    const users = db.collection('users');
+    return users.countDocuments();
+  }
+
+  async nbFiles() {
+    if (!this.isAlive()) {
+      return 0;
+    }
+    const db = this.client.db(this.dbName);
+    const users = db.collection('files');
+    return users.countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
