23489fc7ecb4a54c22c26ace92fcc39f1640f661
Yermehyaw <yermehyaw@gmail.com>
2024-12-11 08:52:35 -0800

    [Fix]: Routes in one file

--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,12 @@ const router = Router();  // Routing object for defining exportable routes
 
 // Import routes defined elsewhere i.e in controller/
 import AppController from '../controllers/AppController.js';  // AppController is a class
+import UsersController from '../controllers/UsersController.js';
 
 // Add them to the router
 router.use('/status', AppController.getStatus);
 router.use('/stats', AppController.getStats);
+router.use('/users', UsersController.js);
 
 // export the router, so that it can be imported nad used by the express.js app
 export default router;
